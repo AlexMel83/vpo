ef903434c80e11f93d467ce99de58530ecff7cb3
Alexander Meleshko <meleshko.a.n@ukr.net>
2025-08-03 18:36:05 +0300

    +

--- app/components/header/buttons.vue
+++ app/components/header/buttons.vue
@@ -17,6 +17,17 @@ const switchLanguage = (code: "uk" | "en") => {
     localStorage.setItem("nuxt-i18n-preferred-locale", code);
   }
 };
+
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set(_isDark) {
+    colorMode.preference = _isDark ? "dark" : "light";
+  },
+});
 </script>
 
 <template>
@@ -38,5 +49,17 @@ const switchLanguage = (code: "uk" | "en") => {
         :title="$t('header.ui.switch_language')"
       />
     </div>
+    <ClientOnly v-if="!colorMode?.forced">
+      <UButton
+        :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
+        color="neutral"
+        variant="ghost"
+        @click="isDark = !isDark"
+      />
+
+      <template #fallback>
+        <div class="size-8" />
+      </template>
+    </ClientOnly>
   </div>
 </template>
